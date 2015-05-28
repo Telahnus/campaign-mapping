eb74832997bb7aeaf83eeea7af2c4f283cacc4e5
Telahnus <quan.jordan@gmail.com>
2015-05-28 16:14:51 -0400

    working on js and xml of markers

---
+++ map.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html/xml; charset=utf-8"/>
+    <title>Legrica Sector</title>
+    <style type="text/css"></style>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/map.js" type="text/javascript"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="css/map.css"> -->
+</head>
+
+<body onresize="resizeMapDiv()" onload="load()">
+
+    <input id="edit_btn" type="button" onclick="switchEditing()" value="Edit (off)">
+    <input id="save_btn" type="button" onclick="" value="Save">
+
+    <div id="map"></div>
+
+</body>
+
+</html>
